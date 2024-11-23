dfa21c727fc3ba5685330e281e9670125cbe02ad
escadar432 <escadar1610@gmail.com>
2024-11-23 08:33:52 +0200

    show labels on the bug view

--- public/cmps/BugPreview.jsx
+++ public/cmps/BugPreview.jsx
@@ -1,10 +1,14 @@
 
 
-export function BugPreview({bug}) {
+export function BugPreview({ bug }) {
 
     return <article>
         <h4>{bug.title}</h4>
         <h1>🐛</h1>
         <p>Severity: <span>{bug.severity}</span></p>
+        <ul>
+            {bug.labels.map(label => <li key={bug._id+''+label}>{label}</li>)}
+        </ul>
+
     </article>
 }
